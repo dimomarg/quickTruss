b6038accd8489e987c8ea14c03e19151164bd418
dimomarg <dimomarg@gmail.com>
2022-04-05 04:26:37 +0300

    zooming and scrolling properly-ish implemented

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-//Prep Canvas
+//CANVAS SETUP
+
 const canvas = document.getElementById('mainCanvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -6,10 +7,38 @@ const ctx = canvas.getContext('2d');
 
 var scrolling = false;
 
-var mouse = [undefined, undefined];
+mouse = { //mouse handler class
+    coords : [0,0],
+    prevCoords : [0,0],
+    hasMoved : false,
+    
+    update : function(event){
+        this.prevCoords = [this.coords[0], this.coords[1]]; 
+        this.coords = [event.x, event.y];
+        this.hasMoved = true;
+    },
 
-window.addEventListener('mousemove', function(event){
-    mouse = [event.x, event.y]
+    getDelta : function(){
+        if (this.hasMoved){
+            this.hasMoved = false;
+            return [this.coords[0] - this.prevCoords[0],
+                    this.coords[1] - this.prevCoords[1]];
+        }
+        else {
+            return [0,0];
+        }
+    }
+}
+
+canvas.addEventListener('mousemove', function(event){
+    mouse.update(event);
+})
+
+canvas.addEventListener('wheel', function(event){
+    console.log(event);
+    event.preventDefault();
+    console.log(event.deltaY);
+    viewPort.zoom += event.deltaY/10;
 })
 
 class node{
@@ -53,40 +82,58 @@ viewPort = {
     transform : function(coords){
         let newcoords = [0,0];
         newcoords[0] = coords[0] * this.zoom;
-        newcoords[0] -= this.pan[0] * this.zoom;
+        newcoords[0] -= this.pan[0]// * this.zoom;
         newcoords[0] += canvas.width/2;
         
         newcoords[1] = coords[1] * this.zoom;
-        newcoords[1] -= this.pan[1]*this.zoom;
+        newcoords[1] -= this.pan[1]// * this.zoom;
         newcoords[1] += canvas.height/2;
 
         return newcoords;
     }
 }
 
+//TEST CODE
 
 let nodes = [new node([0,0]), new node([1,1])];
 let beams = [new beam([nodes[0], nodes[1]])];
+
 for (let i = 0; i < nodes.length; ++i){
     nodes[i].draw();
 }
+
 for (let i = 0; i < beams.length; ++i){
     beams[i].draw();
 }
 
+function mainLoop(){
+    requestAnimationFrame(mainLoop);
+    let mouseDelta = [0,0];
+
+    if (scrolling){
+        mouseDelta = mouse.getDelta();
+        viewPort.pan[0] -= mouseDelta[0];
+        viewPort.pan[1] -= mouseDelta[1]; //TODO: make mouse class that retrieves mosueDelta.
+        
+    }
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+    for (let i = 0; i < nodes.length; ++i){ //TODO: draw function.
+        nodes[i].draw();
+    }
+    for (let i = 0; i < beams.length; ++i){
+        beams[i].draw();
+    }
+}
+
+mainLoop();
+
 // EVENTS
-canvas.onmousedown = function(event){
+canvas.onmousedown = function(){
     scrolling = true;
 }
 
-canvas.onmouseup = function(event){
+canvas.onmouseup = function(){
     scrolling = false;
-    console.log(mouse);
 }
-
-function loop(){
-    requestAnimationFrame(loop);
-    console.log("looping");
-}
-
-loop();
