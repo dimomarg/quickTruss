342d85cadc2161037d534079c86ddc8edbba73b5
dimomarg <dimomarg@gmail.com>
2022-04-08 05:22:19 +0300

    implemented hover detection functions for nodes and beams

--- main.js
+++ main.js
@@ -54,10 +54,25 @@ class node{
     }
 
     draw(){
-        let drawCoords = viewPort.transform(this.coords);
+        let drawCoords = viewPort.unitsToPixels(this.coords);
         ctx.beginPath();
         ctx.arc(drawCoords[0], drawCoords[1], 10, 0, 2*Math.PI);
+        if (this.isTouchingPoint(mouse.coords)){
+            ctx.fillStyle = "#0000ff";
+        }
+        else{
+            ctx.fillStyle = "#000000"
+        }
         ctx.fill();
+    }
+
+    isTouchingPoint(coords){
+        let distanceVector = viewPort.unitsToPixels(this.coords);
+        distanceVector = [distanceVector[0] - coords[0], distanceVector[1]- coords[1]]
+        let distanceSquared = distanceVector[0]*distanceVector[0]
+                            + distanceVector[1]*distanceVector[1];
+        console.log(distanceVector, distanceSquared);
+        return distanceSquared<100;
     }
 }
 
@@ -66,13 +81,45 @@ class beam{
         this.nodes = nodes;
     }
     draw(){
-        let startCoords = viewPort.transform(this.nodes[0].coords);
-        let endCoords = viewPort.transform(this.nodes[1].coords);
+        let startCoords = viewPort.unitsToPixels(this.nodes[0].coords);
+        let endCoords = viewPort.unitsToPixels(this.nodes[1].coords);
         ctx.beginPath();
         ctx.moveTo(startCoords[0], startCoords[1]);
         ctx.lineTo(endCoords[0], endCoords[1]);
         ctx.lineWidth = 5;
+        if (this.isTouchingPoint(mouse.coords)){
+            ctx.strokeStyle = "#FF0000"
+        }
+            else{
+                ctx.strokeStyle = "#000000"
+            }
         ctx.stroke();
+    }
+    
+    isTouchingPoint(c){ //caution: returns infinity if point is closest to end points
+        let a = viewPort.unitsToPixels(this.nodes[0].coords);
+        let b = viewPort.unitsToPixels(this.nodes[1].coords);
+
+        let ab = [b[0] - a[0], b[1]-a[1]]; //vectors ab, ac and bc
+        let bc = [c[0] - b[0], c[1]-b[1]];
+        let ac = [c[0] - a[0], c[1]-a[1]];
+        
+        let ab_bc = ab[0] * bc[0] + ab[1] * bc[1]; //dot products
+        let ab_ac = ab[0] * ac[0] + ab[1] * ac[1];
+
+        if (ab_bc >0){
+            return false;
+        }
+        
+        else if(ab_ac <0){
+            return false;
+        }
+        
+        else{
+            let temp = Math.abs((ab[0] * ab[1] + ac[0] * ac[1]));
+            return (Math.abs(ab[0] * ac[1] - ac[0] * ab[1]) / temp < 0.18);
+        }
+
     }
 }
 
@@ -80,7 +127,7 @@ viewPort = {
     pan:[0, 0], //center of the viewPort in length units.
     zoom:100.0, //one length unit is zoom pixels
     
-    transform : function(coords){
+    unitsToPixels : function(coords){
         let newcoords = [0,0];
         newcoords[0] = coords[0] * this.zoom;
         newcoords[0] -= this.pan[0]// * this.zoom;
@@ -93,7 +140,7 @@ viewPort = {
         return newcoords;
     },
     
-   reverseTransform : function(coords){
+   pixelsToUnits : function(coords){
        let newcoords = [0,0];
        newcoords[0] = coords[0] - canvas.width/2;
        newcoords[0] += this.pan[0];
@@ -102,6 +149,8 @@ viewPort = {
        newcoords[1] = coords[1] - canvas.height/2;
        newcoords[1] += this.pan[1];
        newcoords[1] /= this.zoom;
+       
+       return newcoords;
    }
 }
 
@@ -148,4 +197,5 @@ canvas.onmousedown = function(){
 
 canvas.onmouseup = function(){
     scrolling = false;
+    console.log(beams[0].isTouchingPoint(mouse.coords));
 }
